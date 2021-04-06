47bd71e20d5ec73bb220833fd78413b3ef7d6828
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-04-06 15:18:32 +0100

    Top up method added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,5 +8,12 @@ it "can show a balance" do
   expect(subject).to respond_to(:balance)
 end
 
+it { expect(subject).to respond_to(:top_up).with(1).argument }
+
+it "enables you to top up by 5 pounds" do
+  subject.top_up(5)
+  expect(subject.balance).to eq(5)
+end  
+
 
 end
