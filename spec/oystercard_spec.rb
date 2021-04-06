7c8f59fcc35c840c1a69e73961db07306c22167a
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-04-06 18:12:55 +0100

    Refactored

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,51 +2,51 @@ require 'oystercard'
 
 describe Oystercard do
 
-it { is_expected.to be_an_instance_of Oystercard }
+  it { is_expected.to be_an_instance_of Oystercard }
 
-it "can show a balance" do
-  expect(subject).to respond_to(:balance)
-end
+  it "can show a balance" do
+    expect(subject).to respond_to(:balance)
+  end
 
-it { expect(subject).to respond_to(:top_up).with(1).argument }
+  it { expect(subject).to respond_to(:top_up).with(1).argument }
 
-it "enables you to top up by 5 pounds" do
-  subject.top_up(5)
-  expect(subject.balance).to eq(5)
-end
+  it "enables you to top up by 5 pounds" do
+    subject.top_up(5)
+    expect(subject.balance).to eq(5)
+  end
 
-it "does not allow more than £90 to be topped up" do
-  max_balance = Oystercard::MAX_BALANCE
-  subject.top_up(max_balance)
-  expect { subject.top_up(1) }.to raise_error "max balance #{max_balance} reached"
-end
+  it "does not allow more than £90 to be topped up" do
+    subject.top_up(Oystercard::MAX_BALANCE)
+    expect { subject.top_up(1) }.to raise_error "max balance #{Oystercard::MAX_BALANCE} reached"
+  end
 
-it "does not allow a intial value over £90 to be topped up" do
-  max_value = Oystercard::MAX_BALANCE
-  expect { subject.top_up(91) }.to raise_error "top up value can not be over #{max_value}"
-end
+  it "does not allow a intial value over £90 to be topped up" do
+    expect { subject.top_up(91) }.to raise_error "top up value can not be over #{Oystercard::MAX_BALANCE}"
+  end
 
-it {is_expected.to respond_to(:deduct).with(1).argument }
+  it {is_expected.to respond_to(:deduct).with(1).argument }
 
-it "deducts 10 pounds" do
-  subject.top_up(15)
-  subject.deduct(10)
-  expect(subject.balance).to eq(5)
-end  
+  it "deducts 10 pounds" do
+    subject.top_up(15)
+    subject.deduct(10)
+    expect(subject.balance).to eq(5)
+  end  
 
-it { is_expected.to respond_to(:touch_in) }
+  it { is_expected.to respond_to(:touch_in) }
 
-it "returns a true value when card touches in" do
-  subject.touch_in
-  expect(subject).to be_in_journey
-end
+  it "returns a true value when card touches in" do
+    subject.top_up(Oystercard::MAX_BALANCE)
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
 
-it { is_expected.to respond_to(:touch_out) }
+  it { is_expected.to respond_to(:touch_out) }
 
-it "returns a true value when card touches out" do
-  subject.touch_in
-  subject.touch_out
-  expect(subject).not_to be_in_journey
-end
+  it "returns a true value when card touches out" do
+    subject.top_up(Oystercard::MAX_BALANCE)
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+  end
 
 end
